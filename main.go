78b065061bc816337b7d89f97811fca715e4fef6
maytees <mateespublicprofile@gmail.com>
2025-12-18 03:44:50 +0300

    feat: working (hardcoded) answers

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func cliWelcome() {
-	fmt.Println("Welcome to the DENIS DNS server!")
+	fmt.Println("Denis DNS server started.")
 }
 
 type ServerConfig struct {
@@ -39,7 +39,7 @@ func ParseHeader(header []byte) Header {
 func main() {
 	cliWelcome()
 
-	udpAddress, err := net.ResolveUDPAddr("udp", "127.0.0.1:5354")
+	udpAddress, err := net.ResolveUDPAddr("udp", "127.0.0.1:53")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -67,15 +67,17 @@ func main() {
 		rawHeader := message[:offset]
 
 		header := ParseHeader(rawHeader)
-		fmt.Printf("\nHeader: \n\tID: %v\n\tFlags: %v\n\tQDCOUNT (Question): %v\n\tANCOUNT (Answer): %v\n\tNSCOUNT (Authority): %v\n\tARCOUNT (Additional): %v\n\t\n",
-			header.ID,
-			header.FLAGS,
-			header.QDCount,
-			header.ANCount,
-			header.NSCount,
-			header.ARCount)
+		// fmt.Printf("\nHeader: \n\tID: %v\n\tFlags: %v\n\tQDCOUNT (Question): %v\n\tANCOUNT (Answer): %v\n\tNSCOUNT (Authority): %v\n\tARCOUNT (Additional): %v\n\t\n",
+		// 	header.ID,
+		// 	header.FLAGS,
+		// 	header.QDCount,
+		// 	header.ANCount,
+		// 	header.NSCount,
+		// 	header.ARCount)
 
 		qNameMap := []string{}
+		nameStart := offset
+		nameEnd := -1
 
 		for {
 			length := message[offset]
@@ -85,31 +87,131 @@ func main() {
 
 			if message[offset] == 0 {
 				offset += 1 // moves off the 0
+				nameEnd = offset
 				break
 			}
 		}
 
 		resolvedDomain := strings.Join(qNameMap, ".")
+		nameLabels := message[nameStart:nameEnd]
+		fmt.Printf("Resolved byte name: %x\n", nameLabels)
 		fmt.Println("Resolved Domain:", resolvedDomain)
 
-		qType := binary.BigEndian.Uint16(message[offset : offset+2])
+		// qType := binary.BigEndian.Uint16(message[offset : offset+2])
 		offset += 2
 
-		qClass := binary.BigEndian.Uint16(message[offset : offset+2])
+		// qClass := binary.BigEndian.Uint16(message[offset : offset+2])
 		offset += 2
 
-		fmt.Printf("QType: %x\nQClass: %x\n", qType, qClass)
+		// fmt.Printf("QType: %x\nQClass: %x\n", qType, qClass)
 
-		fmt.Printf("Sending response back...\n")
-
-		// responseData := make([]byte, 512)
-		// x, err := connection.WriteToUDP(responseData, udpAddress)
-		// if err != nil {
-		// 	log.Print("Error occured when sending response:", err)
-		// }
-
-		fmt.Printf("Whole: %x\n", message)
-		fmt.Printf("Question?: %x\n", message[offset:])
-		fmt.Printf("Answer?: %x\n", message[:offset])
+		// Offset sent here plain beacuse it's the end of the question
+		// TODO: If later on authority and additional are implemented before this call
+		// Use another var, since offset would be different
+		SendAnswer(connection, clientAddr, &header, message, offset, nameLabels)
 	}
+}
+
+// TODO: Add options
+func composeFlag(queryFlags uint16) (answerFlags uint16) {
+	var flags uint16 = 0
+
+	// QR, 1 for response, 0 for query
+	flags |= 1 << 15
+
+	// Opcode, 4 bits, 15 - 4 = 11, 0 = standard query
+	flags |= 0 << 11
+
+	// AA, not sure, set to 0
+	flags |= 0 << 10
+	// TC, not sure, set to 0
+	flags |= 0 << 9
+
+	// RD, copies bit at pos 8 (RD) from queryFlags
+	flags |= (queryFlags >> 8) & 1 << 8
+
+	// RA, recursion available
+	flags |= 1 << 7
+
+	// Z, empty, so is this line necessary?
+	flags |= 0 << 4
+
+	// TODO: have different status', keep as no error for now (0).
+	flags |= 0 // no need to do <<0
+
+	return flags
+}
+
+func SendAnswer(connection *net.UDPConn,
+	clientAddress *net.UDPAddr,
+	queryHeader *Header,
+	message []byte,
+	questionEndOffset int,
+	nameLabels []byte,
+) {
+	response := make([]byte, 512)
+	offset := 0
+
+	// Forming header, 12 bytes
+	binary.BigEndian.PutUint16(response[offset:], queryHeader.ID)
+	offset += 2
+
+	flags := composeFlag(queryHeader.FLAGS)
+	binary.BigEndian.PutUint16(response[offset:], flags)
+	offset += 2
+
+	// 1 Question
+	binary.BigEndian.PutUint16(response[offset:], 1)
+	offset += 2
+
+	// 1 Answer
+	binary.BigEndian.PutUint16(response[offset:], 1)
+	offset += 2
+
+	// No Authority
+	binary.BigEndian.PutUint16(response[offset:], 0)
+	offset += 2
+
+	// No Additional
+	binary.BigEndian.PutUint16(response[offset:], 0)
+	offset += 2
+
+	// Question section, just copy from original query
+	copy(response[offset:], message[12:questionEndOffset])
+	offset += questionEndOffset - 12
+
+	// Answer - TODO: Don't hardcode
+	copy(response[offset:], nameLabels)
+	offset += len(nameLabels)
+
+	// Hard coded A record
+	binary.BigEndian.PutUint16(response[offset:], 1)
+	offset += 2
+
+	// Hard coded IN
+	binary.BigEndian.PutUint16(response[offset:], 1)
+	offset += 2
+
+	// Hard coded 0 cache TTL
+	binary.BigEndian.PutUint32(response[offset:], 0)
+	offset += 4 // TODO: 4 or 2?
+
+	binary.BigEndian.PutUint16(response[offset:], 4)
+	offset += 2
+
+	response[offset] = 127
+	response[offset+1] = 0
+	response[offset+2] = 0
+	response[offset+3] = 2
+	offset += 4
+
+	fmt.Printf("\nResponse: %x\n", response[:offset])
+
+	// The colon before the offset removes all the empty stuff after
+	_, err := connection.WriteToUDP(response[:offset], clientAddress)
+	if err != nil {
+		log.Print("Error occured when sending response:", err)
+	}
+
+	fmt.Println("Sent response back.")
 }
